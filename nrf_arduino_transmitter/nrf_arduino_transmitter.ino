9fe2be19c35d970e0dc2a0d0e8a553c698055f34
Natarajan <rajagopn@685b359c3667.ant.amazon.com>
2014-05-10 13:08:22 +0530

    transmitter and user info stubs

---
+++ nrf_arduino_transmitter/nrf_arduino_transmitter.ino
@@ -0,0 +1,35 @@
+#include <SoftwareSerial.h>
+// For the electronic wiring , you should :
+// Connect pinRx to the Pin2 of XBee(Tx , Dout)
+// Connect pinTx to the Pin3 of XBee(Rx , Din)
+
+// Define the pins on Arduino for XBee comminication
+uint8_t pinRx = 2 , pinTx = 4; // the pin on Arduino
+long BaudRate = 9600 , sysTick = 0;
+char GotChar;
+// Initialize NewSoftSerial
+SoftwareSerial mySerial( pinRx , pinTx );
+
+void setup()  
+{ // You shall see these messages in Arduino Serial Monitor
+// This part is the official library , it will be used for talking to
+// PC serial port
+  Serial.begin(BaudRate);
+  Serial.println("XBee Communication Test Start !");
+  Serial.print("BaudRate:");
+  Serial.println(BaudRate);
+  Serial.print("NewSoftSerial Rx Pin#");
+  Serial.println(pinRx,DEC);
+  Serial.print("NewSoftSerial Tx Pin#");
+  Serial.println(pinTx,DEC);
+
+  // This part is the NewSoftSerial for talking to XBee
+  mySerial.begin(BaudRate);
+  mySerial.println("Powered by NewSoftSerial !");
+}
+
+void loop()                    
+{
+      mySerial.print(2);
+      delay(5000);
+}
